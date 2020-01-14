f954cff0af94eb48a6512c4008367db1bc0b330f
Vadym <vadikdeputat@gmail.com>
2020-01-14 17:38:26 +0100

    Add files via upload

--- LiczbaPierwsza/main.cpp
+++ LiczbaPierwsza/main.cpp
@@ -2,24 +2,30 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+bool pierwsza(int);
+
 int main(int argc, char** argv) {
-	int liczba;
-	
-	if(liczba < 2) {
-		printf("Nie jest pierwsza");
-	}
-	
-	scanf("%d", &liczba);
-	
-	for(int i = 2; i * i <= liczba; i++) {
-		if(liczba % i == 0) {
-			printf("Nie jest pierwsza");
-			
-		} else {
-			printf("Liczba: %d jest pierwsza", liczba);
-		}
-	}
-	
-	
-	return 0;
+    int liczba;
+    std::cin >> liczba;
+
+    if(pierwsza(liczba)) {
+        std::cout << liczba << " jest pierwsza";
+    }
+    else
+        std::cout << liczba << " nie jest pierwsza";
+
+    return 0;
+}
+
+bool pierwsza(int liczba) {
+    if(liczba < 2) {
+        return false;
+    }
+
+    for(int i = 2; i * i <= liczba; i++) {
+        if(liczba % i == 0)
+            return false;
+
+        return true;
+    }
 }
