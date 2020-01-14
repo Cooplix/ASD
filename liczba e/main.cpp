b876604da4d70dba194f296ca9b96e13f6ac6cc6
Vadym <vadikdeputat@gmail.com>
2020-01-14 22:38:44 +0100

    Update main.cpp

--- liczba e/main.cpp
+++ liczba e/main.cpp
@@ -9,7 +9,7 @@ double E(double);
 
 
 int main() {
-    cout << "Podaj dokladnosc";
+    cout << "Podaj dokladnosc: ";
     double esp;
     std::cin >> esp;
     cout << setprecision(esp + 1) << E(esp);
